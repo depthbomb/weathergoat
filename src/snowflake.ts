5d679eb7ecf257fb9c99a09554f23d7c33824d43
depthbomb <depthbomb@users.noreply.github.com>
2023-10-10 18:51:45 -0500

    refactor: do not export snowflake epoch

--- src/snowflake.ts
+++ src/snowflake.ts
@@ -1,4 +1,5 @@
 import { Snowflake } from '@sapphire/snowflake';
 
-export const epoch     = new Date('2022-08-16T00:00:00-0600');
-export const snowflake = new Snowflake(epoch);
+export const snowflake = new Snowflake(
+	new Date('2022-08-16T00:00:00-0600')
+);
