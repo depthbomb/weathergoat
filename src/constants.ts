d81a7e8db8ded65247f8a3aa31fa466749e44e76
depthbomb <depthbomb@users.noreply.github.com>
2023-10-10 18:51:57 -0500

    refactor: rename data directory

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export const IS_MACOS = ['macos', 'darwin'].includes(platform);
 
 export const ROOT_DIR     = __dirname;
 export const PROJECT_ROOT = join(ROOT_DIR, '..');
-export const DATA_DIR     = join(PROJECT_ROOT, '.weathergoat');
+export const DATA_DIR     = join(PROJECT_ROOT, '.data');
 export const STORE_DIR    = join(DATA_DIR, 'store');
 export const LOGS_DIR     = join(DATA_DIR, 'logs');
 export const CONFIG_PATH  = join(PROJECT_ROOT, 'config.toml');
