440960f025c9660bfb738b57fa8e541ddefb0090
depthbomb <depthbomb@users.noreply.github.com>
2024-02-08 06:06:09 -0600

    refactor: update GitHubService

--- README.md
+++ README.md
@@ -4,6 +4,37 @@
 
 ---
 
-Discord bot for reporting weather alerts and hourly forecasts to channels built with Discord.Net and the National Weather Service API.
+Discord bot for reporting weather alerts and hourly forecasts to channels. Built with Discord.Net and the National Weather Service API.
 
-More to come.
+## Self-Hosting
+
+Self-hosting is as easy as filling out a config and running the bot binary! The config file looks like the following:
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+    <OwnerId>your user ID</OwnerId>
+    <SentryDsn>your Sentry DSN for error reporting (leave blank to disable)</SentryDsn>
+    <Services>
+        <!--
+            This group only applies to the global version of WeatherGoat to retrieve
+            the latest git commit hash or if you're just wanting to retrieve it
+            yourself.
+
+            Basically, keep it disabled unless you know what you're doing.
+        -->
+        <GitHub>
+            <Enabled>False</Enabled>
+            <Username>your GitHub username</Username>
+            <Repository>repository name</Repository>
+            <Token>GitHub token</Token>
+        </GitHub>
+    </Services>
+</configuration>
+```
+
+Save the config as **weathergoat.xml** and place it in the same directory as the bot binary. To get the bot binary you can either clone the repo and compile it yourself or download it from the [releases.](https://github.com/depthbomb/WeatherGoatNext/releases/latest)
+
+## Usage
+
+Coming soon!
--- WeatherGoat/Services/GitHubService.cs
+++ WeatherGoat/Services/GitHubService.cs
@@ -6,10 +6,11 @@ namespace WeatherGoat.Services;
 
 public class GitHubService
 {
-    public const string CacheKey = "GitHubService.LatestCommitHash";
+    public const string CacheKey = "LatestCommitHash";
     
     private readonly ILogger<GitHubService> _logger;
     private readonly IMemoryCache           _cache;
+    private readonly bool                   _enabled;
     private readonly string                 _username;
     private readonly string                 _repo;
     private readonly GitHubClient           _client;
@@ -19,25 +20,35 @@ public class GitHubService
     {
         _logger   = logger;
         _cache    = cache;
+        _enabled  = config.GetValue<bool?>("Services:GitHub:Enabled") is true;
         _username = config.GetValue<string>("Services:GitHub:Username");
         _repo     = config.GetValue<string>("Services:GitHub:Repository");
-        var token = config.GetValue<string>("Services:GitHub:Token");
-        if (_username == null || _repo == null || token == null)
-        {
-            throw new Exception("Missing GitHub Service configuration value(s)");
-        }
 
-        _client = new GitHubClient(new ProductHeaderValue("WeatherGoatNext"))
+        if (_enabled)
         {
-            Credentials = new Credentials(token)
-        };
+            var token = config.GetValue<string>("Services:GitHub:Token");
+            if (_username == null || _repo == null || token == null)
+            {
+                throw new Exception("Missing GitHub Service configuration value(s)");
+            }
+
+            _client = new GitHubClient(new ProductHeaderValue("WeatherGoatNext"))
+            {
+                Credentials = new Credentials(token)
+            };
+        }
         _semaphore = new SemaphoreSlim(1, 1);
     }
 
     public async Task<string?> GetLatestCommitHashAsync(CancellationToken ct = default)
     {
-        await _semaphore.WaitAsync(ct);
+        if (!_enabled)
+        {
+            return null;
+        }
         
+        await _semaphore.WaitAsync(ct);
+
         try
         {
             if (_cache.TryGetValue<string>(CacheKey, out var hash))
