9e0f734d9cfd1cd325ed09e9de81080155234c74
depthbomb <depthbomb@users.noreply.github.com>
2024-02-07 23:44:57 -0600

    refactor: remove hardcoded version

--- WeatherGoat.Shared/Constants.cs
+++ WeatherGoat.Shared/Constants.cs
@@ -4,20 +4,19 @@ namespace WeatherGoat.Shared;
 
 public static class Constants
 {
-    public static readonly DateTime StartDate = DateTime.Now;
+    public const ulong CreatorId = 133325534548590594;
     
-    private const          int     VersionMajor    = 1;
-    private const          int     VersionMinor    = 3;
-    private const          int     VersionPatch    = 0;
-    private const          int     VersionRevision = 0;
-    public static readonly Version Version         = new(VersionMajor, VersionMinor, VersionPatch, VersionRevision);
-
-    public static readonly string AlertWebhookName = "WeatherGoat#Alerts";
-    public static readonly string BrowserUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
-    public static readonly string BotUserAgent     = $"WeatherGoat {Version}";
+    public static readonly DateTime StartDate = DateTime.Now;
 
     public static readonly bool IsLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
 
+    #region Strings
+    public static readonly string AlertWebhookName = "WeatherGoat#Alerts";
+    public static readonly string BrowserUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+    public static readonly string BotUserAgent     = $"WeatherGoat (github.com/depthbomb/WeatherGoatNext)";
+    #endregion
+
+    #region Filesystem
     public static string BotDirectory = Path.GetFullPath(".");
     public static string BotDataDirectory
     {
@@ -40,4 +39,5 @@ public static class Constants
     public static readonly string DatabaseFilePath  = Path.Combine(DatabaseDirectory, "weathergoat.db");
     public static readonly string LogFilePath       = Path.Combine(LogsDirectory, "weathergoat-.log");
     public static readonly string ConfigFilePath    = Path.Combine(BotDirectory, "weathergoat.xml");
+    #endregion
 }
