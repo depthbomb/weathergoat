f536239c310b15d63ff3b92bc1b1a84cdb5cf3c1
depthbomb <depthbomb@users.noreply.github.com>
2024-02-07 23:45:14 -0600

    refactor: update UpdateStatusJob

--- WeatherGoat/Jobs/UpdateStatusJob.cs
+++ WeatherGoat/Jobs/UpdateStatusJob.cs
@@ -1,6 +1,7 @@
 ﻿using Quartz;
 using Humanizer;
 using System.Text;
+using System.Reflection;
 using WeatherGoat.Services;
 
 namespace WeatherGoat.Jobs;
@@ -9,11 +10,13 @@ public class UpdateStatusJob : IJob
 {
     private readonly DiscordSocketClient _client;
     private readonly GitHubService       _github;
+    private readonly Version?            _assemblyVersion;
 
     public UpdateStatusJob(DiscordSocketClient client, GitHubService github)
     {
-        _client = client;
-        _github = github;
+        _client          = client;
+        _github          = github;
+        _assemblyVersion = Assembly.GetEntryAssembly()?.GetName().Version;
     }
 
     #region Implementation of IJob
@@ -26,17 +29,28 @@ public class UpdateStatusJob : IJob
             await _client.SetStatusAsync(UserStatus.DoNotDisturb);
         }
 
-        var sb = new StringBuilder();
-        sb.Append("Forecasting for ")
-          .Append(DateTime.Now.Subtract(Constants.StartDate).Humanize(3))
-          .Append(" | v")
-          .Append(Constants.Version);
-
+        var sb = new StringBuilder().Append("Forecasting for ")
+                                    .Append(DateTime.Now.Subtract(Constants.StartDate).Humanize(3));
         var hash = await _github.GetLatestCommitHashAsync(ct);
-        if (hash != null)
+
+        if (_assemblyVersion != null)
         {
-            sb.Append('/')
-              .Append(hash);
+            sb.Append(" (v")
+              .Append(_assemblyVersion);
+            
+            if (hash != null)
+            {
+                sb.Append('/')
+                  .Append(hash);
+            }
+
+            sb.Append(')');
+        }
+        else if (hash != null)
+        {
+            sb.Append(" (")
+              .Append(hash)
+              .Append(')');
         }
 
         await _client.SetCustomStatusAsync(sb.ToString());
