c0a43fd58e7a3f45f3c7c0fb9d3397fa0a5df5c3
depthbomb <depthbomb@users.noreply.github.com>
2024-05-11 01:05:04 -0500

    feat: stop jobs on client destroy

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,8 +1,8 @@
 import { db } from '@db';
-import Cron from 'croner';
 import { logger } from '@lib/logger';
 import { captureError } from '@lib/errors';
 import { init } from '@paralleldrive/cuid2';
+import { Cron, scheduledJobs } from 'croner';
 import { Client, Collection } from 'discord.js';
 import { JOBS_DIR, EVENTS_DIR, COMMANDS_DIR } from '@constants';
 import { findFilesRecursivelyRegex } from '@sapphire/node-utilities';
@@ -45,6 +45,12 @@ export class WeatherGoat<T extends boolean> extends Client<T> {
 	}
 
 	public async destroy() {
+		for (const job of scheduledJobs) {
+			if (!job.isStopped()) {
+				job.stop();
+			}
+		}
+
 		await db.$disconnect();
 		if (!logger.closed) {
 			logger.close();
