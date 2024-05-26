4f47db7de961b14048dbf7f2a4976705144e3341
depthbomb <depthbomb@users.noreply.github.com>
2024-05-25 23:25:08 -0500

    feat: add responses logging to `HttpClient`

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,6 +1,9 @@
 import { logger } from '@lib/logger';
+import { hrtime } from 'node:process';
 import { joinURL, withQuery } from 'ufo';
+import { init } from '@paralleldrive/cuid2';
 import { BOT_USER_AGENT } from '@constants';
+import { DurationFormatter } from '@sapphire/time-utilities';
 import { retry, handleResultType, ExponentialBackoff } from 'cockatiel';
 import type { QueryObject } from 'ufo';
 import type { RetryPolicy } from 'cockatiel';
@@ -19,21 +22,23 @@ type RequestOptions = RequestInit & { query?: QueryObject };
 type GETOptions  = Omit<RequestOptions, 'method'>;
 
 export class HttpClient {
-	private readonly _retry:       boolean;
-	private readonly _baseUrl?:    string;
-	private readonly _retryPolicy: RetryPolicy;
+	private readonly _retry:             boolean;
+	private readonly _baseUrl?:          string;
+	private readonly _retryPolicy:       RetryPolicy;
+	private readonly _generateId:        () => string;
+	private readonly _durationFormatter: DurationFormatter;
 
 	public constructor(options?: CreateHttpClientOptions) {
-		this._retry       = !!options?.retry;
-		this._baseUrl     = options?.baseUrl;
+		this._retry = !!options?.retry;
+		this._baseUrl = options?.baseUrl;
 		this._retryPolicy = retry(handleResultType(Response, (res) => res.status !== 200), { maxAttempts: 10, backoff: new ExponentialBackoff() });
+		this._generateId = init({ length: 6 });
+		this._durationFormatter = new DurationFormatter()
 	}
 
 	public async get(url: string | URL, options?: GETOptions): Promise<Response> {
 		return this._doRequest(url, { method: 'GET', ...options });
 	}
-
-	// rewrite the function below to add retry logic please. you may proceed:
 
 	private async _doRequest(input: string | URL, init?: RequestOptions): Promise<Response> {
 		if (typeof input !== 'string') {
@@ -54,16 +59,34 @@ export class HttpClient {
 			requestUrl = withQuery(requestUrl, init.query);
 		}
 
+		const id = this._generateId();
+
 		logger.http('Making HTTP request', {
+			id,
 			method: init?.method,
 			url: requestUrl,
 			retry: this._retry
 		});
 
+		const startTime = hrtime.bigint();
+
+		let res: Promise<Response>;
 		if (this._retry) {
-			return this._retryPolicy.execute(() => fetch(requestUrl, requestInit));
+			res = this._retryPolicy.execute(() => fetch(requestUrl, requestInit));
+		} else {
+			res = fetch(requestUrl, requestInit);
 		}
 
-		return fetch(requestUrl, requestInit);
+		const endTime = hrtime.bigint();
+
+		logger.http('Finished HTTP request', {
+			id,
+			method: init?.method,
+			url: requestUrl,
+			retry: this._retry,
+			elapsed: this._durationFormatter.format(Number((endTime - startTime) / 1000000n))
+		});
+
+		return res;
 	}
 }
