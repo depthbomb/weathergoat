ceaf2a48cee683ac7e5bd0672ce7fef69dd21741
depthbomb <depthbomb@users.noreply.github.com>
2024-05-28 20:03:54 -0500

    chore: remove unused models

--- src/models/glossary.ts
+++
@@ -1,7 +0,0 @@
-import { Type } from 'class-transformer';
-import { GlossaryEntry } from '@models/glossary-entry';
-
-export class Glossary {
-	@Type(() => GlossaryEntry)
-	public glossary!: GlossaryEntry[];
-}
