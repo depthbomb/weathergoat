ee18414b8ffec9067ab37e198f94fc7dd6ad5100
depthbomb <depthbomb@users.noreply.github.com>
2024-06-02 22:16:42 -0500

    refactor: rewrite Git utils

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,24 +1,44 @@
 import { $ } from 'bun';
+import { Type, Expose, plainToInstance } from 'class-transformer';
 
-let hash: string | null = null;
-
-export async function getCurrentCommitHash() {
-	if (!hash) {
-		try {
-			const out = await $`git rev-parse --short HEAD`.text();
-
-			hash = out.trim();
-		} catch {
-			hash = 'INDEV';
-		}
-	}
-
-	return hash;
+class CommitMessage {
+	public hash!: string;
+	@Expose({ name: 'author_name' })
+	public authorName!: string;
+	public message!: string;
+	@Type(() => Date)
+	public date!: Date;
 }
 
-export async function getCommitMessages(count: number): Promise<string[]> {
-	const out   = await $`git log -${count} --pretty=%B`.text();
-	const lines = out.trim().split(/\n\s*\n/);
+export class Git {
+	private static _lastHash: string | null = null;
 
-	return lines;
+	private constructor() {}
+
+	public static async getCurrentCommitHash() {
+		if (!this._lastHash) {
+			try {
+				const out = await $`git rev-parse --short HEAD`.text();
+
+				this._lastHash = out.trim();
+			} catch {
+				this._lastHash = 'INDEV';
+			}
+		}
+
+		return this._lastHash;
+	}
+
+	public static async getCommitMessages(count: number | null = null) {
+		let res: string;
+		if (!count || count < 1) {
+			res = await $`git log --pretty="{\"hash\":\"%H\",\"author_name\":\"%cn\",\"message\":\"%s\",\"date\":\"%cd\"}"`.text();
+		} else {
+			res = await $`git log -${count} --pretty="{\"hash\":\"%H\",\"author_name\":\"%cn\",\"message\":\"%s\",\"date\":\"%cd\"}"`.text();
+		}
+
+		const json = `[${res.trim().split('\n').map(line => line).join()}]`;
+
+		return (JSON.parse(json) as object[]).map(v => plainToInstance(CommitMessage, v));
+	}
 }
