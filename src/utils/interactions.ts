794a68fe42b4b59a9a3c034672f1c24f49921fdc
depthbomb <depthbomb@users.noreply.github.com>
2024-06-02 21:58:06 -0500

    fix: `tryToRespond` not working properly

--- src/utils/interactions.ts
+++ src/utils/interactions.ts
@@ -2,15 +2,17 @@ import type { CommandInteraction, InteractionReplyOptions } from 'discord.js';
 
 export async function tryToRespond(interaction: CommandInteraction, options: string | InteractionReplyOptions) {
 	const { replied, deferred } = interaction;
-	if (!deferred) {
-		if (replied) {
-			if (typeof options === 'string') {
-				return interaction.followUp({ content: options, fetchReply: true });
-			}
-
-			return interaction.followUp({ ...options, fetchReply: true });
-		}
+	if (deferred) {
+		return interaction.editReply(options);
 	}
 
-	return interaction.editReply(options);
+	if (replied) {
+		if (typeof options === 'string') {
+			return interaction.followUp({ content: options, fetchReply: true });
+		}
+
+		return interaction.followUp({ ...options, fetchReply: true });
+	}
+
+	return interaction.reply(options);
 }
