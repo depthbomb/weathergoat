89cdbd65d51c50bf0a8e3dc0624d2dd3f5a5c8b9
depthbomb <depthbomb@users.noreply.github.com>
2024-06-05 21:10:15 -0500

    refactor: merge error utils with lib

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,5 +1,6 @@
 import { logger } from '@lib/logger';
 import { MakeErrorClass } from 'fejl';
+import { DiscordAPIError } from 'discord.js';
 import { captureException } from '@sentry/bun';
 
 export type WeatherGoatError = InvalidPermissionsError;
@@ -15,4 +16,27 @@ export function captureError(message: string, err: unknown, context?: object) {
 
 	logger.error(message, { ...context, errorMessage, stack });
 	captureException(err);
+}
+
+export function isDiscordAPIError(err: unknown): err is DiscordAPIError {
+	return err instanceof DiscordAPIError;
+}
+
+export function isDiscordAPIErrorCode(err: unknown, code: number): boolean;
+export function isDiscordAPIErrorCode(err: unknown, codes: number[]): boolean;
+export function isDiscordAPIErrorCode(err: unknown, oneOrMoreCodes: number | number[]): boolean {
+	if (isDiscordAPIError(err)) {
+		let { code: errorCode } = err;
+		if (typeof oneOrMoreCodes === 'number') {
+			return errorCode === oneOrMoreCodes;
+		} else {
+			if (typeof errorCode === 'string') {
+				errorCode = parseInt(errorCode);
+			}
+
+			return oneOrMoreCodes.includes(errorCode);
+		}
+	}
+
+	return false;
 }
