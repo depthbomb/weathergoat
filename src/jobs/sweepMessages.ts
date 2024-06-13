d0624f84bb8f843b45c4ac880f8bde5ace36eedd
depthbomb <depthbomb@users.noreply.github.com>
2024-06-13 00:02:09 -0500

    refactor: remove experiments

--- src/jobs/sweepMessages.ts
+++ src/jobs/sweepMessages.ts
@@ -1,27 +1,16 @@
 import { db } from '@db';
 import { captureError } from '@lib/errors';
-import { queueService } from '@services/queue';
 import { featuresService } from '@services/features';
 import { isTextChannel } from '@sapphire/discord.js-utilities';
 import type { IJob } from '@jobs';
-import type { Queue } from '@services/queue';
 import type { WeatherGoat } from '@lib/client';
-import type { Message, Awaitable } from 'discord.js';
 
-type SweepMessagesQueue = (message: Message<true>) => Awaitable<unknown>;
-
-interface ISweepMessagesJob extends IJob {
-	[kQueue]: Queue<SweepMessagesQueue>;
-}
-
-const kQueue = Symbol('queue');
+interface ISweepMessagesJob extends IJob {}
 
 export const sweepMessagesJob: ISweepMessagesJob = ({
 	name: 'com.weathergoat.jobs.SweepMessages',
 	pattern: '* * * * *',
 	runImmediately: true,
-
-	[kQueue]: queueService.createQueue('com.weathergoat.queues.MessageSweeper', '1s'),
 
 	async execute(client: WeatherGoat<true>) {
 		if (featuresService.isFeatureEnabled('com.weathergoat.features.DisableMessageSweeping', false)) return;
@@ -42,11 +31,7 @@ export const sweepMessagesJob: ISweepMessagesJob = ({
 				if (isTextChannel(channel)) {
 					const message = await channel.messages.fetch(messageId);
 					if (message) {
-						if (featuresService.isFeatureEnabled('com.weathergoat.features.experiments.UseMessageSweeperQueue', false)) {
-							this[kQueue].enqueue(async () => await message.delete());
-						} else {
-							await message.delete();
-						}
+						await message.delete();
 					}
 				}
 			} catch (err) {
