4e7e0bc7ef93b3c749e18c8a83586d72008e5bcd
depthbomb <depthbomb@users.noreply.github.com>
2024-06-30 03:46:12 -0500

    chore: rename auto-radar extension module file

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,13 +2,13 @@ import { PrismaClient } from '@prisma/client';
 
 // Extensions
 import exists from './extensions/exists';
-import radarChannelCountByGuild from './extensions/radar-channel-count-by-guild';
+import autoRadarCountByGuild from './extensions/auto-radar-count-by-guild';
 import alertDestinationCountByGuild from './extensions/alert-destination-count-by-guild';
 import forecastDestinationCountByGuild from './extensions/forecast-destination-count-by-guild';
 
 export const db = new PrismaClient()
 	.$extends(exists)
-	.$extends(radarChannelCountByGuild)
+	.$extends(autoRadarCountByGuild)
 	.$extends(alertDestinationCountByGuild)
 	.$extends(forecastDestinationCountByGuild);
 
