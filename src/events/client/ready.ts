0c8cea826de251e955339e8ad35b9f56dbb1ac9a
depthbomb <depthbomb@users.noreply.github.com>
2024-07-05 21:08:24 -0500

    refactor: separate starting on-ready job logic

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,27 +1,35 @@
 import { BaseEvent } from '@events';
+import { tokens } from '@container';
 import { logger, reportError } from '@logger';
 import type { Logger } from 'winston';
 import type { WeatherGoat } from '@client';
+import type { Container } from '@container';
 
 export default class ClientReadyEvent extends BaseEvent<'ready'> {
+	private readonly _client: WeatherGoat<true>;
 	private readonly _logger: Logger;
 
-	public constructor() {
+	public constructor(container: Container) {
 		super({ name: 'ready' });
 
+		this._client = container.resolve(tokens.client);
 		this._logger = logger.child({ discordEvent: this.name });
 	}
 
 	public async handle(client: WeatherGoat<true>) {
-		const { jobs, readyAt } = client;
+		const { readyAt } = client;
 
 		this._logger.info('Logged in to Discord', { readyAt });
 
-		// Iterate through jobs that should execute or start when the client is ready.
+		await this._startReadyJobs();
+	}
+
+	private async _startReadyJobs() {
+		const { jobs } = this._client;
 		for (const { job, cron } of jobs.filter(({ job }) => job.waitUntilReady)) {
 			if (job.runImmediately) {
 				try {
-					await job.execute(client, cron);
+					await job.execute(this._client, cron);
 				} catch (err) {
 					reportError('Error in `runImmediately`, `waitUntilReady` job', err, { name: job.name });
 				}
