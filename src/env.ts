eebce596b1a77e76c4da6f7b23dfb1581ac74a9f
depthbomb <depthbomb@users.noreply.github.com>
2025-11-21 23:17:56 -0600

    chore: update dependencies

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ export const env = Env.create({
 	MODE: Env.schema.enum(['production', 'development'] as const, { defaultValue: 'development' }),
 	BOT_ID: Env.schema.string(),
 	BOT_TOKEN: Env.schema.string(),
-	DATABASE_URL: Env.schema.string(),
+	DATABASE_URL: Env.schema.url(),
 	SENTRY_DSN: Env.schema.string({ required: false }),
 	GITHUB_ACCESS_TOKEN: Env.schema.string(),
 	MAX_RADAR_MESSAGES_PER_GUILD: Env.schema.int(),
