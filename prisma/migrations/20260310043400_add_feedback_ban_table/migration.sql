2885856f38971b52093b7d92d4467680de4a847d
depthbomb <depthbomb@users.noreply.github.com>
2026-03-10 00:22:25 -0500

    fix: adjust migrations

--- prisma/migrations/20260310043400_add_feedback_ban_table/migration.sql
+++ prisma/migrations/20260310043400_add_feedback_ban_table/migration.sql
@@ -5,7 +5,7 @@ DROP INDEX "VolatileMessage_guildId_idx";
 CREATE TABLE "FeedbackBan" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "userId" TEXT NOT NULL,
-    "reason" TEXT NOT NULL,
+    "reason" TEXT,
     "active" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
