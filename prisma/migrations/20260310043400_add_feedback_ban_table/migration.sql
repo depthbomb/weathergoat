8cb44e13cc8d81413c60e9a27221b394646b0575
depthbomb <depthbomb@users.noreply.github.com>
2026-03-10 00:18:39 -0500

    fix: missing migration

---
+++ prisma/migrations/20260310043400_add_feedback_ban_table/migration.sql
@@ -0,0 +1,12 @@
+-- DropIndex
+DROP INDEX "VolatileMessage_guildId_idx";
+
+-- CreateTable
+CREATE TABLE "FeedbackBan" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" TEXT NOT NULL,
+    "reason" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
